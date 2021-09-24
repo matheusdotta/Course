be54490915ed72a3cba5adc0d96fcbc8eee3135c
Matheus Dotta <matheus_dotta@live.com>
2021-09-24 19:47:40 -0300

    Inplementação de Usuarios

---
+++ src/main/java/com/example/Course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.example.Course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findall() {
+		User u = new User(1L, "Maria", "maria@live.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+
+	}
+}
