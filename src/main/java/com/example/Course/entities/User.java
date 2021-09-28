723c4c37cd4126068f30fa6df5db5c6e8c304c6b
Matheus Dotta <matheus_dotta@live.com>
2021-09-28 20:35:08 -0300

    Implementação do banco de dados

--- src/main/java/com/example/Course/entities/User.java
+++ src/main/java/com/example/Course/entities/User.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 //
 @Entity
@@ -11,6 +14,10 @@ import javax.persistence.Entity;
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private String name;
 	private String email;
